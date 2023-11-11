dcb7f2cdda8a6cce00b13d13d2880c1fae18f612
istewart2 <iain.stewart@kpmg.co.uk>
2023-11-11 15:34:04 +0000

    Setup entity framework

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,13 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
+
+var connectionString = builder.Configuration.GetConnectionString("GameStoreContext");
+
+builder.Services.AddSqlServer<GameStoreContext>(connectionString);
 
 var app = builder.Build();
 
