9bcd462d8a700b0faa2e5d07e37b9b9ed370cef3
istewart2 <iain.stewart@kpmg.co.uk>
2023-11-13 17:13:02 +0000

    Add EF games repository and use this

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,13 +1,8 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
-using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
-
-var connectionString = builder.Configuration.GetConnectionString("GameStoreContext");
-
-builder.Services.AddSqlServer<GameStoreContext>(connectionString);
+builder.Services.AddRepositories(builder.Configuration);
 
 var app = builder.Build();
 
